c3a33d2e5dcfc4cf1c04cf3f986a993588955372
nevadasaga <nevada.saga@gmail.com>
2022-10-11 15:25:09 +0700

    .xml file path changed

--- Volve_Visualizer.py
+++ Volve_Visualizer.py
@@ -6,4 +6,4 @@ import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 
 ## Opening the F-15 well Trajectory file
-WITSML_file = r"C:\Users\Ankit Bansal\Desktop\Volve\Volve - Real Time drilling data 13.05.2018\sitecom14.statoil.no\Norway-Statoil-NO 15_$47$_9-F-15\1\trajectory\1.xml"
+WITSML_file = r"C:\Users\Nevada\Desktop\1.xml"
