bc7f8282b2e4b1c2765dc90c1ec037995abdc597
nevadasaga <nevada.saga@gmail.com>
2022-10-11 15:41:39 +0700

    Read inputed WITSML.xml

--- Volve_Visualizer.py
+++ Volve_Visualizer.py
@@ -5,5 +5,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 
-## Opening the F-15 well Trajectory file
+## Opening the F-4 well Trajectory file
 WITSML_file = r"C:\Users\Nevada\Desktop\1.xml"
+
+# Reading the WITSML file
+with open(WITSML_file) as f:
+    data = f.read()
